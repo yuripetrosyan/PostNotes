e6b9de95d4ef26949e388e90dd99e17125cb1eed
Yuri <yuriyuri200117@gmail.com>
2024-03-23 20:53:34 +0100

    Initial Commit

---
+++ PostNotes/PostNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostNotesApp.swift
+//  PostNotes
+//
+//  Created by Yuri Petrosyan on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PostNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
