064f8d5638b11463f916a36a90aac32603df4662
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-04-06 19:19:48 +0200

    searchbar

---
+++ PostNotes/Custom /SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  PostNotes
+//
+//  Created by Yuri Petrosyan on 06/04/2024.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
