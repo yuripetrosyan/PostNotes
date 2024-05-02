7afa20fb74d14230baef031b48b584377a26bf2e
yuripetrosyan <yuriyuri200117@gmail.com>
2024-05-02 22:59:02 +0200

    Add checkUserExists and login/pass

--- PostNotes/Authentication/AuthService.swift
+++ PostNotes/Authentication/AuthService.swift
@@ -11,12 +11,27 @@ import FirebaseAuth
 
 class AuthService {
     
-    func checkUserExists() {
-        
+    let db = Firestore.firestore() //Access our database
+
+    
+    //Check if a user with a specified email exists in our database
+    func checkUserExists(email: String) async throws -> Bool{
+        let documentSnapshot = db.collection("users").whereField("email", isEqualTo: email).count
+        let count = try await documentSnapshot.getAggregation(source: .server).count
+        return Int(truncating: count) > 0
     }
     
-    func login() {
+    func login(email: String, password: String, userExists: Bool) async throws -> AuthDataResult? {
+        
+        guard !password.isEmpty else { return nil }
+        
+        if userExists {
+            return try await Auth.auth().signIn(withEmail: email, password: password)
+        } else {
+            return try await Auth.auth().createUser(withEmail: email, password: password)
+        }
         
     }
     
 }
+                                                                                          
