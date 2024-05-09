19f2f922a5da001bbf61bc994463acef7c85ee73
yuripetrosyan <yuriyuri200117@gmail.com>
2024-05-09 12:38:58 +0200

    Fix AuthView

--- PostNotes/Authentication/AuthView.swift
+++ PostNotes/Authentication/AuthView.swift
@@ -42,98 +42,89 @@ struct AuthView: View {
             
             
             
-            CustomTextField()
-                .overlay{
-                    TextField("Email", text: $viewModel.emailText)
-                        .textContentType(.username)
-                        .textInputAutocapitalization(.never)
-                        .padding(.horizontal)
-                        .padding(.top, 10)
-                }
+           
             
             if viewModel.isPasswordVisible {
-                CustomTextField()
-                    .overlay{
-                        SecureField("password", text: $viewModel.passwordText)
-                            .textContentType(.password)
-                            .textInputAutocapitalization(.never)
+                VStack{
+                    CustomTextField()
+                        .overlay{
+                            TextField("Email", text: $viewModel.emailText)
+                                .textContentType(.username)
+                                .textInputAutocapitalization(.never)
+                                .padding(.horizontal)
+                                .padding(.top, 10)
+                        }
+                    
+                    CustomTextField()
+                        .overlay{
+                            SecureField("password", text: $viewModel.passwordText)
+                                .textContentType(.password)
+                                .textInputAutocapitalization(.never)
+                                .padding(.horizontal)
+                                .padding(.top, 10)
+                        }
+                    
+                    Button(action: {
+                        viewModel.authenticate(appState: appState)
+                        
+                    },label: {
+                        Text(viewModel.userExists ? "Log In" : "Sign Up")
+                        
+                            .fontWeight(.semibold)
+                            .foregroundColor(.primary1)
+                            .padding()
+                            .frame(width: 150, height:  50)
+                            .background(Color.brandPrimary)
+                            .cornerRadius(30)
                             .padding(.horizontal)
-                            .padding(.top, 10)
-                    }
-                
+                            .shadow(radius: 5, x: 3, y: 3)
+                        
+                    }).padding()
+                }
                 
             }else{
-//                Button(action: {
-//                    viewModel.authenticate(appState: appState)
-//                    
-//                },label: {
-//                    Image(systemName: "arrow.right")
-//                        .fontWeight(.semibold)
-//                        .foregroundColor(.primary1)
-//                        .padding()
-//                        .frame(width: 50, height:  50)
-//                        .background(Color.brandPrimary)
-//                        .cornerRadius(30)
-//                        .padding(.horizontal)
-//                        .shadow(radius: 5, x: 3, y: 3)
-//                }).padding()
+                
+                HStack{
+                    CustomTextField()
+                        .overlay{
+                            TextField("Email", text: $viewModel.emailText)
+                                .textContentType(.username)
+                                .textInputAutocapitalization(.never)
+                                .padding(.horizontal)
+                                .padding(.top, 10)
+                        }
+                    Button(action: {
+                        viewModel.authenticate(appState: appState)
+                        
+                    },label: {
+                        Image(systemName: "arrow.right")
+                        
+                            .fontWeight(.semibold)
+                            .foregroundColor(.primary1)
+                            .frame(width: 40, height:  40)
+                            .background(Color.brandPrimary)
+                            .cornerRadius(30)
+                            .shadow(radius: 5, x: 3, y: 3)
+                    })  .padding(.top, 10)
+                }.padding()
             }
             
             
             if viewModel.isLoading {
                 ProgressView()
             } else{
-                Button(action: {
-                    viewModel.authenticate(appState: appState)
-                    
-                },label: {
-                    Text(viewModel.userExists ? "Log In" : "Sign Up")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.primary1)
-                        .padding()
-                        .frame(width: 150, height:  50)
-                        .background(Color.brandPrimary)
-                        .cornerRadius(30)
-                        .padding(.horizontal)
-                        .shadow(radius: 5, x: 3, y: 3)
-                }).padding()
+               
             }
             
             
             
-            //                HStack(spacing: 5){
-            //                    Text("No account?")
-            //                        .font(.footnote)
-            //
-            //                        Text("Create one!")
-            //                            .fontWeight(.semibold)
-            //                            .font(.footnote)
-            //                            .onTapGesture {
-            //                                withAnimation(Animation.smooth){
-            //                                    sheetIsOn.toggle()
-            //
-            //                                }
-            //                            }
-            //                    }
-            
+      
             
         }
         .padding(.top, 40)
         .padding(.horizontal)
         
     }
-        
-        
-        //            if sheetIsOn{
-        //                FlatSignUpView(sheetIsOn: $sheetIsOn)
-        //                    .transition(.move(edge: .bottom))
-        //                    
-        //            }
-        
-        
-        
-        
-        
     }
 }
 
--- PostNotes/Views/Components/CustomTextField.swift
+++ PostNotes/Views/Components/CustomTextField.swift
@@ -10,11 +10,15 @@ import SwiftUI
 struct CustomTextField: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 25.0)
+        
               //  .strokeBorder(Color.brandPrimary, lineWidth: 2)
                 .frame(width: 300, height: 50)
+                .padding(.top, 10)
                 .foregroundStyle(.ultraThinMaterial.shadow(.inner(radius: 2)))
                 .shadow(radius: 10, x: 4, y: 6)
-                .padding(.top, 10)
+             
+        
+        
     }
 }
 
