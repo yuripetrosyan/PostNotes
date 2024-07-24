7a45803f766d1fbe1d2ecab023a3a3777e18f559
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-07-24 19:20:28 +0200

    Update DetailedView

---
+++ PostNotes/Views/Components/ButtonMenu/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  PostNotes
+//
+//  Created by Yuri Petrosyan on 24/07/2024.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
---
+++ PostNotes/Views/Components/ButtonMenu/PlusView.swift
@@ -0,0 +1,18 @@
+//
+//  PlusView.swift
+//  PostNotes
+//
+//  Created by Yuri Petrosyan on 24/07/2024.
+//
+
+import SwiftUI
+
+struct PlusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlusView()
+}
